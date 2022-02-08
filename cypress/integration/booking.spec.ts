a2db11d21a0f10300085efb91d0a923e999bbb37
willydownhouse <ville.l.alatalo@gmail.com>
2022-02-08 18:55:41 +0100

    test_fix

--- cypress/integration/booking.spec.ts
+++ cypress/integration/booking.spec.ts
@@ -15,9 +15,9 @@ describe('Booking tests', () => {
     cy.get('#burger-menu').click();
     cy.get('#menu-booking').click();
 
-    cy.get('#trip-dropdown').select(3);
+    cy.get('#trip-dropdown').select('Finale MTB week');
 
-    cy.get('#date-dropdown').select(1);
+    cy.get('#date-dropdown').select('7/5/22');
 
     cy.get('#btn-booking').click();
 
